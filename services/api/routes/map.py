44be476da329a6bed3d0fddc89b05b5e36661758
Daniel Pintard <danielpintard@yahoo.com>
2025-01-25 08:28:05 -0500

    removed region from map.py

--- services/api/routes/map.py
+++ services/api/routes/map.py
@@ -6,16 +6,12 @@ router = APIRouter()
 
 @router.get("/map-data")
 async def get_map_data(
-    region: str = Query(None, description="Region name, in this case Denver"),
     bound_box: str = Query(None, description="Bounding box in 'min_lon,min_lat,max_lon,max_lat'")
 ):
-    if not region and not bound_box:
-        raise HTTPException(
-            status_code=400,
-            detail="Requires 'region' or 'bound_box' parameter" )
-    
     try:
-        map_data = fetch_map_data(region, bound_box)
+        map_data = fetch_map_data(bound_box)
         return JSONResponse(content=map_data)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str("An unexpected error occurred."))
