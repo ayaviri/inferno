58bebc0d9567468ceffd03f6ca0dac476def9d02
Daniel Pintard <danielpintard@yahoo.com>
2025-01-25 08:59:07 -0500

    imported fetch_data func into main

--- services/api/routes/map.py
+++ services/api/routes/map.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Query, HTTPException
 from fastapi.responses import JSONResponse
-# import fetch_map_data function from 
+from services.map_service import fetch_map_data
 
 router = APIRouter()
 
@@ -8,6 +8,10 @@ router = APIRouter()
 async def get_map_data(
     bound_box: str = Query(None, description="Bounding box in 'min_lon,min_lat,max_lon,max_lat'")
 ):
+    """
+    Takes Bounding box as input for region (i.e. Denver)
+    Returns Map Data
+    """
     try:
         map_data = fetch_map_data(bound_box)
         return JSONResponse(content=map_data)
@@ -15,3 +19,5 @@ async def get_map_data(
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str("An unexpected error occurred."))
+
+
