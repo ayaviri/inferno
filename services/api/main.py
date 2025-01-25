3e7f922aad629121b4c9d3f1e36a6329aa4acc98
Daniel Pintard <danielpintard@yahoo.com>
2025-01-25 08:22:52 -0500

    edited main func

--- services/api/main.py
+++ services/api/main.py
@@ -5,4 +5,6 @@ app = FastAPI()
 
 app.include_router(map_router, prefix="/api")
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
