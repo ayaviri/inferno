407c65aec17022d70b0307d7bae763008c53347f
Daniel Pintard <danielpintard@yahoo.com>
2025-01-25 10:27:08 -0500

    yml file

--- services/api/services/simulate_response_time.py
+++ services/api/services/simulate_response_time.py
@@ -2,5 +2,7 @@ import random
 import math 
 from typing import List, Tuple
 
-def simulate_response_time(em_stations, num_emergencies):
-    pass
+def simulate_response_time(em_stations, num_emergencies = 5):
+    emergencies = []
+    for _ in range():
+        pass 
