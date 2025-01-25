b409f4d7175762c8fdd257a59fe8cc488f017bdb
Daniel Pintard <danielpintard@yahoo.com>
2025-01-25 12:49:35 -0500

    ready to integrate graphhopper

--- services/apiv2/main.py
+++ services/apiv2/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi import APIRouter, Query, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from shapely.geometry import shape, Point
+import json
+import random
 #from services.map_service import fetch_map_data
 
 app = FastAPI()
@@ -14,24 +17,52 @@ def test():
     return {'message': 'Hello World'}
 
 @app.post('/avg-response-time')
-async def compute_avg_res(
+def compute_avg_res(
     body: ComputeAvgResBody
 ):
     em_locs: list[list[float]] = region_chunks()
+    total = 0
     
     for loc in em_locs:
-        total += compute_fastest_time_to_loc(loc, body.response_locs)
+        time = compute_fastest_time_to_loc(loc, body.response_locs)
+        total += time
+        print(time)
     
     compute_avg = total / len(em_locs) 
     
-    return {'average': compute_avg}
+    return {'average': compute_avg} 
 
 def region_chunks():
-    pass
+    # convention for project - lon, lat
+    with open('denver.geojson', 'r') as geojson_file:
+        geojson_data = json.load(geojson_file)
+    
+    
+    random_coords = []
+    
+    for feature in geojson_data['features']:
+        geom = shape(feature['geometry'])
+        minx, miny, maxx, maxy = geom.bounds
+        n = 2
+        feature_coords = []
+        
+        while len(feature_coords) < n:
+            lat = random.uniform(miny, maxy)
+            lon = random.uniform(minx, maxx)
+            point = Point(lon, lat)
+            if geom.contains(point):
+                feature_coords.append([lon, lat])
+
+        random_coords.extend(feature_coords)
+    return random_coords
+            
 
 def compute_fastest_time_to_loc(loc: list[float], response_ctrs: list[list[float]]):
-    pass
+    return min([compute_response_time(loc, ctr) for ctr in response_ctrs])
 
+
+def compute_response_time(loc: list[float], response_ctrs: list[float]):
+    return random.randint(1,5)
 
 @app.get("/map-data")
 async def get_map_data(
@@ -54,8 +85,5 @@ async def get_map_data(
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
