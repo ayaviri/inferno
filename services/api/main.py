11b628dbb9d2df9f9baea4618e5284b4f9b8e3e5
ayaviri <joshuayaviri@gmail.com>
2025-01-26 07:12:24 -0500

    Added a script to precompute optimal configurations for each of the values in the slider range. They are now served without explicitly starting a simulation, as the value is just being fetched from a newly upped Redis instance

--- services/api/main.py
+++ services/api/main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from shapely.geometry import shape, Point
+import redis
 import json
 import random
 import requests
@@ -20,6 +21,23 @@ app.add_middleware(
     allow_methods=["GET", "POST", "DELETE"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("startup")
+def startup():
+    global r
+    r = redis.Redis(host="localhost", port=6379, db=0)
+
+
+class SimulateSingleRequestBody(BaseModel):
+    fire_loc: list[float]
+    response_locs: list[list[float]]
+
+
+@app.post("/simulate-single")
+def simulate_single_fire(body: SimulateSingleRequestBody):
+    response_time = compute_fastest_time_to_loc(body.fire_loc, body.response_locs)
+    return {"response_time": response_time}
 
 
 class ComputeAvgResBody(BaseModel):
@@ -107,16 +125,30 @@ def compute_response_time(loc: list[float], response_ctrs: list[float]):
 
     return travel_time
 
+
 @app.get("/optimal-config")
 def optimal_config(n: int):
-    center_loc: list[list[float]] = compute_optimal_config(n)
-    return {"center_loc" : center_loc}
+    byte_data = r.get(str(n))
+    center_locs = convert_to_coordinate_pairs(byte_data)
+    return {"center_locs": center_locs}
+
+
+def convert_to_coordinate_pairs(byte_data: bytes):
+    coordinates = json.loads(byte_data.decode("utf-8"))
+    return [list(map(float, pair)) for pair in coordinates]
+
+
+@app.get("/optimal-config-offline")
+def optimal_config_offline(n: int):
+    center_locs: list[list[float]] = compute_optimal_config(n)
+    return {"center_locs": center_locs}
+
 
 def compute_optimal_config(n: int):
     fire_locations = region_chunks(50)
     init_centroids = initialize_centroids(fire_locations, n)
     centroids = init_centroids
-    
+
     for i in range(20):
         print(i)
         clusters = {i: [] for i in range(n)}
@@ -130,97 +162,33 @@ def compute_optimal_config(n: int):
                     route_times += [999999999]
             closest_station = np.argmin(route_times)
             clusters[closest_station].append(fire)
-            
+
         new_centroids = []
         for cluster_points in clusters.values():
             new_centroids.append(find_centroid(cluster_points))
-        
+
         if stabilized(centroids, new_centroids):
             break
         centroids = new_centroids
-        
+
     return centroids
+
 
 def initialize_centroids(fire_loc: list[list[float]], n: int):
     return random.sample(fire_loc, n)
+
 
 def find_centroid(cluster):
     lon_coords = [pt[0] for pt in cluster]
     lat_coords = [pt[1] for pt in cluster]
     mean_lon = np.mean(lon_coords)
     mean_lat = np.mean(lat_coords)
-    
+
     return [mean_lon, mean_lat]
+
 
 def stabilized(old: list[list[float]], new: list[list[float]]) -> bool:
     total_abs_dist = 0
     for old, new in zip(old, new):
         total_abs_dist = math.dist(old, new)
-    return total_abs_dist < 0.001 
-
-
-    
-
-# def validate_and_snap_loc(location: list[float]):
-#     GRAPH_HOPPER_API_URL = ("http://localhost:8989/route") # Q: this is passed as a tuple right? Also we should maybe globablize this
-    
-#     response = requests.get(GRAPH_HOPPER_API_URL, params = {
-#         "point" : f"{location[0]},{location[1]}",
-#         "point" : f"{location[0]},{location[1]}", #overwriting first point with same point to request one
-#         "vehicle" : "car",
-#         "locale" : "en",
-#         "snap_prevention" : ["ferry", "tunnel", "bridge"] # Q /j: wtf is a "ford" lol
-#     })
-#     response.raise_for_status()
-#     gh_response = response.json()
-    
-#     if "paths" in gh_response and len(gh_response["paths"]) > 0:
-#         snapped_point = gh_response["paths"][0]["snapped_waypoints"]
-#         return [snapped_point["lon"], snapped_point["lat"]]
-#     else:
-#         raise ValueError("No valid roads near the given location") # Q: is raising an error totally necessary?
-    
-#     # Q: will RequestException be necessary
-
-# def optimize_response_ctrs (emergencies: list[list[float]], num_ctrs: int):
-#     kmeans = KMeans(n_clusters = num_ctrs, random_state = 42).fit(emergencies)
-#     centroids = kmeans.cluster_centers_
-    
-#     snapped_locs = []
-#     for centroid in centroids:
-#         snapped_location = validate_and_snap_loc(centroid)
-#         snapped_locs.append(snapped_locs)
-    
-#     return snapped_locs
-
-# def eval_ctr_config(emergencies: list[list[float]], response_centers: list[list[float]]):
-#     total_response_time = 0
-#     for emergency in emergencies:
-#         min_time = min([compute_response_time(emergency, center) for center in response_centers])
-#         total_response_time += min_time
-    
-#     avg_resp_time = total_response_time / len(emergencies)
-#     return avg_resp_time
-
-@app.get("/map-data")
-async def get_map_data(
-    bbox: str = Query(
-        "...", description="Bounding box in 'min_lon,min_lat,max_lon,max_lat'"
-    ),
-):
-    """
-    Takes Bounding box as input for region (i.e. Denver)
-    Returns Map Data
-    """
-
-    try:
-        map_data = fetch_map_data(bbox)
-        return JSONResponse(content=map_data)
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=str(e))
-    except FileNotFoundError as e:
-        raise HTTPException(status_code=404, detail=f"Data not found: {e}")
-    except Exception:
-        raise HTTPException(
-            status_code=500, detail=str("An unexpected error occurred.")
-        )
+    return total_abs_dist < 0.001
